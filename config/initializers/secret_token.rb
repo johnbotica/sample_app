1cd97fb9c66155b952e13f4d2741c85b0e3ecab2
John Botica <john@digital-telepathy.com>
2012-12-20 10:13:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'dbb9ba71ebf4298d15ff249b89ac283cfd085a781fabbb00bdfff4805a0b51edcd78b619d2b2ffab4e45811b03e1b2d72af1a869c0d6847e9f587d4d552def6e'
