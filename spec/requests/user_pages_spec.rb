d7b6d13f01d421af1558d402f70631ec541f64e5
John Botica <john@digital-telepathy.com>
2013-02-01 11:28:42 -0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,7 +33,7 @@ describe "User pages" do
       describe "after submission" do
         before { click_button submit }
         
-        it { should have_selector('title', text: 'Sign up') }       
+        it { should have_selector('title', text: 'Sign Up') }       
         it { should have_content('error') }       
       end
     end
@@ -56,6 +56,7 @@ describe "User pages" do
         
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
